499fc501c4eda58ee79c0b252ae228e5eb7afb52
yobelyuantomo <yobelyuantomo@gmail.com>
2020-10-28 12:47:02 +0800

    Master File - VueBasic_9715

--- vue.js
+++ vue.js
@@ -7,7 +7,7 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global = global || self, global.Vue = factory());
-}(this, function () { 'use strict';
+}(this, function () { 'use strict'; 
 
   /*  */
 
